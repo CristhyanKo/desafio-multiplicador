e21af3c21be0364295bdcf9421141a2f3eb718fe
Cristhyan Kohlhase <cristhyan.ko@outlook.com>
2019-01-30 14:49:56 -0300

    Correção gramatical

--- desafio-mutiplicador/Program.cs
+++ desafio-mutiplicador/Program.cs
@@ -7,13 +7,13 @@ namespace desafio_mutiplicador
         static void Main(string[] args)
         {
             Console.WriteLine("=============================================================================");
-            Console.WriteLine("=                        Desafio Mutiplicador - TJMT                        =");
+            Console.WriteLine("=                        Desafio Multiplicador - TJMT                        =");
             Console.WriteLine("=============================================================================");
             Console.WriteLine();
             Console.Write("Objetivo: ");
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("O desafio consiste em fazer um algoritimo que mutiplique 2 números");
-            Console.Write("          inteiros sem o uso do operador de mutiplicação (");
+            Console.WriteLine("O desafio consiste em fazer um algorítimo que multiplique 2 números");
+            Console.Write("          inteiros sem o uso do operador de multiplicação (");
             Console.ForegroundColor = ConsoleColor.Red;
             Console.Write("*");
             Console.ForegroundColor = ConsoleColor.Yellow;
@@ -27,18 +27,18 @@ namespace desafio_mutiplicador
             var secondNumber = Console.ReadLine();
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("A mutiplicação dos dois números é igual a: ");
+            Console.Write("A multiplicação dos dois números é igual a: ");
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write(Calcular(Convert.ToInt32(firstNumber), Convert.ToInt32(secondNumber)));
             Console.ReadKey();
         }
 
-        private static int Mutiplicar(int firstNumber, int secondNumber)
+        private static int Multiplicar(int firstNumber, int secondNumber)
         {
             if (firstNumber == 0 || secondNumber == 0)
                 return firstNumber;
 
-            return firstNumber + Mutiplicar(firstNumber, secondNumber - 1);
+            return firstNumber + Multiplicar(firstNumber, secondNumber - 1);
         }
 
         private static int Calcular(int firstNumber, int secondNumber)
@@ -46,7 +46,7 @@ namespace desafio_mutiplicador
             var firstNumberFix = (firstNumber < 0) ? (0 - (firstNumber)) : firstNumber;
             var secondNumberFix = (secondNumber < 0) ? (0 - (secondNumber)) : secondNumber;
 
-            return Mutiplicar(firstNumberFix, secondNumberFix) - firstNumberFix;
+            return Multiplicar(firstNumberFix, secondNumberFix) - firstNumberFix;
         }
     }
 }
